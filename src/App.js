a126f7dd19985ef37a24b2389015ba97cbc3a79b
Harry Vu <harryvu@outlook.com>
2021-08-28 22:29:17 -0700

    removes NavLink import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  NavLink
+  //NavLink
 } from "react-router-dom";
 import Home from './components/Home'
 import About from './components/About'
